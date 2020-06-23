fb20810af2c296aa7fa4bddab5bc3361d49441d1
Normalist-K <uvento06@hanmail.net>
2020-06-23 15:52:40 +0900

    #5. 서브메뉴, 자유곡선 추가

--- day2/day2_5/Program.cs
+++ day2/day2_5/Program.cs
@@ -74,42 +74,82 @@ namespace day2_5
             return new Circle();
         }
     }
+    class FreeLine : iShape
+    {
+        public FreeLine()
+        {
+            LineList.Add(new Line());
+            LineList.Add(new Line());
+            LineList.Add(new Line());
+            LineList.Add(new Line());
+            LineList.Add(new Line());
+            LineList.Add(new Line());
+        }
+        private List<iShape> LineList = new List<iShape>(); 
+         void iShape.Draw()
+        {
+            Console.WriteLine("자유곡선 그리기 시작");
+            for (int i = 0; i < LineList.Count; i++)
+            {
+                LineList[i].Draw();
+            }
+            Console.WriteLine("자유곡선 그리기 시작");
+        }
+        string iShape.GetShapeName()
+        {
+            return "자유곡선";
+        }
+        iShape iShape.Copy()
+        {
+            return new FreeLine();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             List<iShape> ShapeList = new List<iShape>();
-            List<iShape> ShapeMenuList = new List<iShape>() { new Line(), new Rectangle(), new Triangle(), new Circle() };
+            List<iShape> ShapeMenuList = new List<iShape>() { new Line(), new Rectangle(), new Triangle(), new Circle(), new FreeLine() };
             int iChoice = 1;
             iShape selItem = null;
             while (iChoice != 0)
             {
-                Console.WriteLine("1. 라인 선택");
-                Console.WriteLine("2. 삼각형 선택");
-                Console.WriteLine("3. 사각형 선택");
-                Console.WriteLine("4. 원형 선택");
-                Console.WriteLine("5. 그림판에 그리기");
-                Console.WriteLine("6. 그림판 내용 보기");
+                Console.WriteLine("1. 도형 선택");
+                Console.WriteLine("2. 그림판에 그리기");
+                Console.WriteLine("3. 그림판 내용 보기");
                 iChoice = int.Parse(Console.ReadLine());
 
                 switch (iChoice)
                 {
                     case 1:
-
+                        {
+                            for (int i = 0; i < ShapeMenuList.Count; i++)
+                            {
+                                Console.WriteLine("{0}. {1} 선택", i + 1, ShapeMenuList[i].GetShapeName());
+                            }
+                            int iSelShape = int.Parse(Console.ReadLine());
+                            if (iSelShape > 0 && iSelShape <= ShapeMenuList.Count)
+                            {
+                                selItem = ShapeMenuList[iSelShape - 1];
+                            }
+                            else
+                            {
+                                selItem = null;
+                            }
+                        }
                         break;
                     case 2:
-
+                        if (selItem != null)
+                        {
+                            ShapeList.Add(selItem.Copy());
+                        }
+                        else
+                        {
+                            Console.WriteLine("도형을 선택하세요.");
+                        }
                         break;
                     case 3:
-
-                        break;
-                    case 4:
-                        selItem = ShapeMenuList[iChoice - 1];
-                        break;
-                    case 5:
-                        ShapeList.Add(selItem.Copy());
-                        break;
-                    case 6:
                         {
                             for (int i = 0; i < ShapeList.Count; i++)
                             {
