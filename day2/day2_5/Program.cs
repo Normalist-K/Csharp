929abe14b56cdbbdc7bcd416cf0c5ee7c7c5043e
Normalist-K <uvento06@hanmail.net>
2020-06-23 21:14:50 +0900

    # 그림판 만들기

--- day2/day2_5/Program.cs
+++ day2/day2_5/Program.cs
@@ -106,23 +106,68 @@ namespace day2_5
     }
     class ToolBarMenu
     {
-        public iShape Menu()
+        public void WriteList(List<iShape> DispList)
         {
-            for (int i = 0; i < ShapeMenuList.Count; i++)
+            for (int i = 0; i < DispList.Count; i++)
             {
-                Console.WriteLine("{0}. {1} 선택", i + 1, ShapeMenuList[i].GetShapeName());
-            }
-            int iSelShape = int.Parse(Console.ReadLine());
-            if (iSelShape > 0 && iSelShape <= ShapeMenuList.Count)
-            {
-                return ShapeMenuList[iSelShape - 1];
-            }
-            else
-            {
-                return null;
+                Console.WriteLine("{0}. {1} 선택", i + 1, DispList[i].GetShapeName());
             }
         }
+        public int DispList(List<iShape> ListObj)
+        {
+            WriteList(ListObj);
+            return int.Parse(Console.ReadLine());
+        }
+        public int DispMenu()
+        {
+            WriteList(ShapeMenuList);
+            Console.WriteLine("{0}. 도형 삭제", ShapeMenuList.Count + 1);
+            Console.WriteLine("{0}. 도형 복원", ShapeMenuList.Count + 2);
+            Console.WriteLine("0. 이전 메뉴로");
+            return int.Parse(Console.ReadLine());
+        }
+        private void SwapListData(List<iShape> FromList, List<iShape> ToList, int idx)
+        {
+            ToList.Add(FromList[idx - 1]);
+            FromList.RemoveAt(idx - 1);
+        }
+        private void RemoveMenu()
+        {
+            if (ShapeMenuList.Count == 0)
+            {
+                Console.WriteLine("더 이상 삭제할 도형이 없습니다.");
+                return;
+            }
+            int iRemoveShape = DispList(ShapeMenuList);
+            SwapListData(ShapeMenuList, ShapeMenuBufferList, iRemoveShape);
+        }
+        private void RestoreMenu()
+        {
+            if (ShapeMenuBufferList.Count == 0)
+            {
+                Console.WriteLine("더 이상 복원할 도형이 없습니다.");
+                return;
+            }
+            int iRestoreShape = DispList(ShapeMenuBufferList);
+            SwapListData(ShapeMenuBufferList, ShapeMenuList, iRestoreShape);
+        }
+        public iShape SubMenu()
+        {
+            int iSelShape = DispMenu();
+            while (iSelShape != 0)
+            {
+                if (iSelShape > 0 && iSelShape <= ShapeMenuList.Count)
+                    return ShapeMenuList[iSelShape - 1];
+                else if (iSelShape == ShapeMenuList.Count + 1)
+                    RemoveMenu();
+                else if (iSelShape == ShapeMenuList.Count + 2)
+                    RestoreMenu();
+                iSelShape = DispMenu();
+            }
+            return null;
+        }
         List<iShape> ShapeMenuList = new List<iShape>() { new Line(), new Rectangle(), new Triangle(), new Circle(), new FreeLine() };
+        List<iShape> ShapeMenuBufferList = new List<iShape>();
     }
     class ShapeMgr
     {
@@ -140,7 +185,7 @@ namespace day2_5
                 switch (iChoice)
                 {
                     case 1:
-                        selItem = toolBarMenu.Menu();
+                        selItem = toolBarMenu.SubMenu();
                         break;
                     case 2:
                         InsertShape(selItem);
@@ -154,20 +199,14 @@ namespace day2_5
         private void InsertShape(iShape selItem)
         {
             if (selItem != null)
-            {
                 ShapeList.Add(selItem.Copy());
-            }
             else
-            {
                 Console.WriteLine("도형을 선택하세요.");
-            }
         }
         private void DrawAllShape()
         {
             for (int i = 0; i < ShapeList.Count; i++)
-            {
                 ShapeList[i].Draw();
-            }
         }
         List<iShape> ShapeList = new List<iShape>();
         ToolBarMenu toolBarMenu = new ToolBarMenu();
