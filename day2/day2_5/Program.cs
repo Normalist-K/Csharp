c5b3071eab16b602e097243a87feb25149a5ab90
Normalist-K <uvento06@hanmail.net>
2020-06-23 14:56:35 +0900

    #1. 그림판 만들기 기본 class 생성

---
+++ day2/day2_5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2_5
+{
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Shape Draw...");
+        }
+        public virtual string GetShapeName()
+        {
+            return "Shape";
+        }
+        public virtual Shape Copy()
+        {
+            return new Shape();
+        }
+        
+    }
+    class Line : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("라인 그리기");
+        }
+        public override string GetShapeName()
+        {
+            return "라인";
+        }
+        public override Shape Copy()
+        {
+            return new Line();
+        }
+
+    }
+    class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("삼각형 그리기");
+        }
+        public override string GetShapeName()
+        {
+            return "삼각형";
+        }
+        public override Shape Copy()
+        {
+            return new Triangle();
+        }
+    }
+    class Rectangle : Shape
+    {
+         public override void Draw()
+        {
+            Console.WriteLine("사각형 그리기");
+        }
+        public override string GetShapeName()
+        {
+            return "사각형";
+        }
+        public override Shape Copy()
+        {
+            return new Rectangle();
+        }
+    }
+    class Circle : Shape
+    {
+         public override void Draw()
+        {
+            Console.WriteLine("원형 그리기");
+        }
+        public override string GetShapeName()
+        {
+            return "원형";
+        }
+        public override Shape Copy()
+        {
+            return new Circle();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
