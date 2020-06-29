1ff1ac66f5f3618707a58464faf78be56830a3b3
Normalist-K <uvento06@hanmail.net>
2020-06-29 11:33:58 +0900

    new project

---
+++ 0629/day4/day4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "홍길동 김길동 이길동 박길동";
+            string[] sArray = s.Split(' ');
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}", i, sArray[i]);
+            }
+        }
+    }
+}
