83691edb47235b78b0585f6cab96cdb8c2d9252c
Normalist-K <uvento06@hanmail.net>
2020-06-29 11:41:28 +0900

    #1. ExtentionClass 생성

--- 0629/day4/day4_3/Program.cs
+++ 0629/day4/day4_3/Program.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace day4_3
 {
+    static class ExtentionClass
+    // static class 는 멤버 변수로 static만 가능
+    // 누구나 사용 가능하게 할 때
+    {
+        public static int StringCount(this string s)
+        {
+            return s.Split(' ').Length;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             string s = "홍길동 김길동 이길동 박길동";
+            Console.WriteLine("s.StringCount : {0}", s.StringCount());
             string[] sArray = s.Split(' ');
+            // " " 은 뒤에 null이 붙어있음. ' ' 과 다름 주의
             for (int i = 0; i < sArray.Length; i++)
             {
                 Console.WriteLine("{0}. {1}", i, sArray[i]);
