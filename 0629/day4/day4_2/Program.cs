b44151c14530ae298a4268b4afee44e560a34c39
Normalist-K <uvento06@hanmail.net>
2020-06-29 10:44:11 +0900

    #1. 손코딩

--- 0629/day4/day4_2/Program.cs
+++ 0629/day4/day4_2/Program.cs
@@ -11,13 +11,43 @@ namespace day4_2
         static void Main(string[] args)
         {
             int[] iArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            DispFunc(iArray);
         }
 
         static void DispFunc(int [] iArray)
         {
+            Console.WriteLine("==============================================");
             for (int i = 0; i < iArray.Length; i++)
             {
                 Console.WriteLine("iArray[{0}] = {1}", i, iArray[i]);
+                Console.WriteLine("----------------------------------------------");
+            }
+            Console.WriteLine("==============================================");
+        }
+        static void DispFunc2(int[] iArray)
+        {
+            for (int i = 0; i < iArray.Length; i++)
+            {
+                Console.WriteLine("iArray[{0}] = {1}", i, iArray[i]);
+            }
+        }
+        static void DispFunc3(int[] iArray, int iGubun)
+        {
+            if (iGubun == 1)
+            {
+                Console.WriteLine("==============================================");
+            }
+            for (int i = 0; i < iArray.Length; i++)
+            {
+                Console.WriteLine("iArray[{0}] = {1}", i, iArray[i]);
+                if (iGubun==1)
+                {
+                    Console.WriteLine("---------------------------------------------");
+                }
+            }
+            if (iGubun == 1)
+            {
+                Console.WriteLine("==============================================");
             }
         }
     }
