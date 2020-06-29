da4e04095da605884090fa913a1cf4117b58d728
Normalist-K <uvento06@hanmail.net>
2020-06-29 13:39:12 +0900

    new project #1. 생성자 / 소멸자

---
+++ 0629/day4/day4_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_5
+{
+    class Cat
+    {
+        public Cat()
+        {
+            Console.WriteLine("pulbic Cat()생성자 호출");
+        }
+        //~Cat()
+        //{
+        //    Console.WriteLine("pulbic Cat()소멸자 호출");
+        //}
+        // 소멸자 쓰면 이거 호출하려고 한 사이클 더 돌기 때문에 안써 -> 대신 소멸자 역할하는 함수를 사용
+        public void Dispose()
+        {
+            Console.WriteLine("pulbic Cat()소멸자 호출");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.Dispose();
+        }
+    }
+}
