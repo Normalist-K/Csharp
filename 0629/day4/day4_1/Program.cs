b90f17f67cb268fb4d4f5d98f664820481a25dbf
Normalist-K <uvento06@hanmail.net>
2020-06-29 10:33:20 +0900

    #5. delegate 함수 단점

--- 0629/day4/day4_1/Program.cs
+++ 0629/day4/day4_1/Program.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace day4_1 // delegate
 {
-    delegate void DispFuncDelegate();
+    delegate void DispFuncDelegate(int iPercent);
     // class type, 변수로 함수의 주소가 들어감
+    // delegate 단점: 함수의 type(return type, parameter type)이 동일해야 함
     class DispFuncClass
     {
         public string MenuString;
@@ -16,25 +17,30 @@ namespace day4_1 // delegate
     }
     class Program
     {
-        static void DispArrow()
+        static void DispArrow(int iPercent)
         {
             Console.Write("=>");
         }
-        static void DispStar()
+        static void DispStar(int iPercent)
         {
             Console.Write("*");
         }
-        static void DispPlus()
+        static void DispPlus(int iPercent)
         {
             Console.Write("+");
+        }
+        static void DispPercent(int iPercent)
+        {
+            Console.Write("{0}% 완료", iPercent);
         }
 
         static void Main(string[] args)
         {
-            DispFuncClass[] DispFuncClassArray = new DispFuncClass[3] { 
+            DispFuncClass[] DispFuncClassArray = new DispFuncClass[4] { 
                 new DispFuncClass() { MenuString = "DispArrow", DeleFunc=DispArrow }, 
                 new DispFuncClass() { MenuString = "DispStar", DeleFunc=DispStar }, 
                 new DispFuncClass() { MenuString = "DispPlus", DeleFunc=DispPlus }, 
+                new DispFuncClass() { MenuString = "DispPercent", DeleFunc=DispPercent }, 
             };
             int iChoice = 1;
             while (iChoice != 0)
@@ -53,10 +59,10 @@ namespace day4_1 // delegate
 
         static void MyFunc(DispFuncDelegate DispFunc)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 Thread.Sleep(500);
-                DispFunc();
+                DispFunc(i*10);
                 // 어떤 함수가 실행될 지는 실행시에 결정됨 -> late binding
                 // c/c++ 은 pointer로 late binding
                 // c#/java 는 pointer가 없으니까 delegate 활용 (late binding하려고 virtual쓰기도 함)
