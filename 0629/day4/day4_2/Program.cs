ddbdb18c4686e55a10ad221f5bd749d5e7dea268
Normalist-K <uvento06@hanmail.net>
2020-06-29 10:37:43 +0900

    new project

---
+++ 0629/day4/day4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] iArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+        static void DispFunc(int [] iArray)
+        {
+            for (int i = 0; i < iArray.Length; i++)
+            {
+                Console.WriteLine("iArray[{0}] = {1}", i, iArray[i]);
+            }
+        }
+    }
+}
