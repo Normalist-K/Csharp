306801a6995e732e7db2c0aeffd5c30f4915d9e1
Normalist-K <uvento06@hanmail.net>
2020-06-29 13:46:18 +0900

    new project / 도서관리 프로그램

---
+++ 0629/day4/day4_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_6 // 도서관리 프로그램
+{
+    class Member
+    {
+        public void inputData()
+        {
+            Console.Write("전화번호: ");
+            TelNo = Console.ReadLine();
+            Console.Write("이름: ");
+            Name = Console.ReadLine();
+            Console.Write("주소: ");
+            Addr = Console.ReadLine();
+        }
+        private string TelNo;
+        private string Name;
+        private string Addr;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
