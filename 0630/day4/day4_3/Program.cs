bb2fb3425adebd021875e685a8c23787eec1b13a
Normalist-K <uvento06@hanmail.net>
2020-06-30 11:43:04 +0900

    #1. event로 error 제어하기

--- 0630/day4/day4_3/Program.cs
+++ 0630/day4/day4_3/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace day4_3
 {
+    delegate void DispErrDelegate(string strMessage);
     class Cat
     {
         public int AGE
@@ -15,12 +17,15 @@ namespace day4_3
             {
                 if (value <= 0)
                 {
-                    Console.WriteLine("나이는 1보다 작을 수 없습니다.");
+                    DispErrEvent("나이는 1보다 작을 수 없습니다.");
+                    //Console.WriteLine("나이는 1보다 작을 수 없습니다.");
+                    // 이렇게 하면 콘솔에서 밖에 사용할 수 없다. 사용환경에 맞게 쓸 수 있도록 하려면 event로 정보를 넘겨줘.
                     return;
                 }
                 m_iAge = value;
             }
         }
+        public event DispErrDelegate DispErrEvent;
         private int m_iAge;
     }
     class Program
@@ -28,7 +33,22 @@ namespace day4_3
         static void Main(string[] args)
         {
             Cat c = new Cat();
+            c.DispErrEvent += C_DispErrEvent;
             c.AGE = -1;
+
+            Cat c2 = new Cat();
+            c2.DispErrEvent += C2_DispErrEvent;
+            c2.AGE = -1;
+        }
+
+        private static void C2_DispErrEvent(string strMessage)
+        {
+            MessageBox.Show(strMessage);
+        }
+
+        private static void C_DispErrEvent(string strMessage)
+        {
+            Console.WriteLine("ErrMsg : {0}", strMessage);
         }
     }
 }
