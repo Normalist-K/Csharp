2d89de33435926d84dd84b097c98a060181c1152
Normalist-K <uvento06@hanmail.net>
2020-06-30 12:03:31 +0900

    New project

---
+++ 0630/day4/day4_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "홍길동";
+
+            FileStream f = new FileStream("C://Users//uvent//source//repos//Normalist-K//hustar2//0630", FileMode.Create);
+            //FileStream f = new FileStream(@"C:\Users\uvent\source\repos\Normalist-K\hustar2\0630", FileMode.Create);
+            // 역슬래쉬 쓰는 두가지 방법
+        }
+    }
+}
