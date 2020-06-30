d0825b530e79a464f046d7ac1b712c6476cd60bc
Normalist-K <uvento06@hanmail.net>
2020-06-30 11:32:27 +0900

    New project

---
+++ 0630/day4/day4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_3
+{
+    class Cat
+    {
+        public int AGE
+        {
+            get { return m_iAge; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("나이는 1보다 작을 수 없습니다.");
+                    return;
+                }
+                m_iAge = value;
+            }
+        }
+        private int m_iAge;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.AGE = -1;
+        }
+    }
+}
