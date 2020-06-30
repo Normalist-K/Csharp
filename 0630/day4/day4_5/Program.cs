dbe98d849f952e41256047be73a3f44b1a6cef67
Normalist-K <uvento06@hanmail.net>
2020-06-30 13:15:43 +0900

    #2. 여러 string 저장하기

--- 0630/day4/day4_5/Program.cs
+++ 0630/day4/day4_5/Program.cs
@@ -11,15 +11,26 @@ namespace day4_5
     {
         static void Main(string[] args)
         {
-            string s = "홍길동";
+            string s1 = "홍길동";
+            string s2 = "김길동";
+            string s3 = "박길동";
+
 
             FileStream f = new FileStream("C://Users//uvent//source//repos//Normalist-K//hustar2//0630//day4//day4_5//1.txt", FileMode.Create);
             //FileStream f = new FileStream(@"C:\Users\uvent\source\repos\Normalist-K\hustar2\0630", FileMode.Create);
             // 역슬래쉬 쓰는 두가지 방법
 
             // string을 파일로 저장할 때 byte 배열로 인코딩해야 함
-            byte[] data = Encoding.Default.GetBytes(s);
+            byte[] data = Encoding.Default.GetBytes(s1);
             f.Write(data, 0, data.Length);
+            
+            data = Encoding.Default.GetBytes(s2);
+            f.Write(data, 0, data.Length);
+            
+            data = Encoding.Default.GetBytes(s3);
+            f.Write(data, 0, data.Length);
+
+
             f.Close();
             f.Dispose();
         }
