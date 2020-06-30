36a1f9e6d33f06c0858d385b3491c7721feecae7
Normalist-K <uvento06@hanmail.net>
2020-06-30 09:17:34 +0900

    #2. LINQ with class

--- 0630/day4/day4_1/Program.cs
+++ 0630/day4/day4_1/Program.cs
@@ -8,27 +8,25 @@ namespace day4_1
 {
     class Program
     {
+        class Cat
+        {
+            public int AGE
+            { get; set; }
+            public void PrintData()
+            {
+                Console.WriteLine("AGE: {0}", AGE);
+            }
+        }
         static void Main(string[] args)
         {
-            int[] iArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            for (int i = 0; i < iArray.Length; i++)
-            {
-                Console.WriteLine("iArray[{0}] : {1}", i, iArray[i]);
-            }
-            // 같은 결과
-            foreach (int i in iArray)
-            {
-                Console.WriteLine("iArray : {0}", i);
-            }
-
             // LINQ
             List<int> iList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var v = from item in iList
                     where item % 2 == 0
-                    select item; // new도 가능
+                    select new Cat() { AGE = item };
             foreach (var vItem in v)
             {
-                Console.WriteLine("vItem : {0}", vItem);
+                vItem.PrintData();
             }
         }
     }
