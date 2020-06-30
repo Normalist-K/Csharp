c0f4bda4248bb20b99c250fb757deeaaaf641ec4
Normalist-K <uvento06@hanmail.net>
2020-06-30 12:07:16 +0900

    #1. string을 텍스트 파일로 저장하기

--- 0630/day4/day4_5/Program.cs
+++ 0630/day4/day4_5/Program.cs
@@ -13,9 +13,15 @@ namespace day4_5
         {
             string s = "홍길동";
 
-            FileStream f = new FileStream("C://Users//uvent//source//repos//Normalist-K//hustar2//0630", FileMode.Create);
+            FileStream f = new FileStream("C://Users//uvent//source//repos//Normalist-K//hustar2//0630//day4//day4_5//1.txt", FileMode.Create);
             //FileStream f = new FileStream(@"C:\Users\uvent\source\repos\Normalist-K\hustar2\0630", FileMode.Create);
             // 역슬래쉬 쓰는 두가지 방법
+
+            // string을 파일로 저장할 때 byte 배열로 인코딩해야 함
+            byte[] data = Encoding.Default.GetBytes(s);
+            f.Write(data, 0, data.Length);
+            f.Close();
+            f.Dispose();
         }
     }
 }
