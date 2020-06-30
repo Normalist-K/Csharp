be2f7818bbe2ddcd7eef64764d40d19416113acd
Normalist-K <uvento06@hanmail.net>
2020-06-30 10:44:15 +0900

    #1. IEnumerable, IEnumerator 상속해서 foreach 사용하기

--- 0630/day4/day4_2/Program.cs
+++ 0630/day4/day4_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace day4_2
 {
-    class IntArray
+    class IntArray : IEnumerable<int>, IEnumerator<int>
     {
         public IntArray(int size)
         {
+            m_iCurrent = 0;
             m_size = size;
             m_data = new int[size];
         }
@@ -17,6 +19,11 @@ namespace day4_2
         {
             get { return m_size; }
         }
+
+        public int Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
         public int this[int idx]
         {
             get { return m_data[idx]; }
@@ -24,6 +31,32 @@ namespace day4_2
         }
         private int[] m_data;
         private int m_size;
+        private int m_iCurrent;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
     class Program
     {
@@ -37,6 +70,10 @@ namespace day4_2
             for (int i = 0; i < iArray.SIZE; i++)
             {
                 Console.WriteLine("iArray[{0}] = {1}", i, iArray[i]);
+            }
+            foreach (int i in iArray)
+            {
+                Console.WriteLine("iArrya = {0}", iArray[i]);
             }
         }
     }
